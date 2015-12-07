995d977c5cf40fdf2ab4edcd3b635179933de2f0
Froehlich <Steven.Froehlich@br.de>
2015-12-07 16:15:26 +0100

    update

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -3,11 +3,11 @@ var bio = {
   'name': 'Steven Froehlich',
   'role': 'Frontend Master',
   'contacts': {
-    'mobile': "+49 171 2438468",
-    'email': "steven.froehlich@br.de",
-    'github': "downSiteUp",
-    'twitter': "@Korresponder",
-    'location': "Munich, Germany"
+    'mobile': '+49 171 2438468',
+    'email': 'steven.froehlich@br.de',
+    'github': 'downSiteUp',
+    'twitter': '@Korresponder',
+    'location': 'Munich, Germany'
   },
   'welcomeMessage': 'Es gibt nichts Gutes, ausser man tut es',
   'skills': ['Frontend Entwicklung', 'HTML5', 'CSS3',
@@ -31,41 +31,41 @@ var bio = {
 };
 
 var work = {
-    "jobs": [
+    'jobs': [
         {
-            "employer": "IsarFunk",
-            "title": "Dispatcher",
-            "location": "Munich",
-            "dates": ["01/98 - 12/99"],
-            "description": ["Responsibility for the new GPS-System"]
+            'employer': 'IsarFunk',
+            'title': 'Dispatcher',
+            'location': 'Munich',
+            'dates': '01/98 - 12/99',
+            'description': 'Responsibility for the new GPS-System'
         },
         {
-            "employer": "CDI",
-            "title": "Infromatikkaufmann",
-            "location": "Munich",
-            "dates": ["01/00 - 04/02"],
-            "description": ["Training on the IT-Job"]
+            'employer': 'CDI',
+            'title': 'Infromatikkaufmann',
+            'location': 'Munich',
+            'dates': '01/00 - 04/02',
+            'description': 'Training on the IT-Job'
         },
         {
-            "employer": "ADAC",
-            "title": "Host-System Operator",
-            "location": "Munich",
-            "dates": ["05/02 - 05/05"],
-            "description": ["Data-handling and -programming on selfgenerated M-Text"]
+            'employer': 'ADAC',
+            'title': 'Host-System Operator',
+            'location': 'Munich',
+            'dates': '05/02 - 05/05',
+            'description': 'Data-handling and -programming on selfgenerated M-Text'
         },
         {
-            "employer": "DonwSiteUp",
-            "title": "Web Developer",
-            "location": "Munich",
-            "dates": ["since - 05/06"],
-            "description": ["Lead of WebSite Department"]
+            'employer': 'DonwSiteUp',
+            'title': 'Web Developer',
+            'location': 'Munich',
+            'dates': 'since - 05/06',
+            'description': 'Lead of WebSite Department'
         },
         {
-            "employer": "Bayerischer Rundfunk",
-            "title": "FrontEnd Engeneer",
-            "location": "Munich",
-            "dates": ["since 06/06"],
-            "description": ["FrontEnd Master"]
+            'employer': 'Bayerischer Rundfunk',
+            'title': 'FrontEnd Engeneer',
+            'location': 'Munich',
+            'dates': 'since 06/06',
+            'description': 'FrontEnd Master'
         }
     ],
   'display': function() {
@@ -85,34 +85,34 @@ var work = {
 var projects = {
   'projects': [
       {
-          "title": "BR-Podcastcenter",
-          "dates": "07/06 - 08/07",
-          "description": "Hosting all the BR Podcast-Content on its own plattform",
-          "URL": ["http://www.br-online.de/podcast/"]
+          'title': 'BR-Podcastcenter',
+          'dates': '07/06 - 08/07',
+          'description': 'Hosting all the BR Podcast-Content on its own plattform',
+          'images': 'images/podcastcenter.jpg'
       },
       {
-          "title": "Yuranga",
-          "dates": "12/07 - 08/09",
-          "description": "Launching the new CMS called TeamSite for BR.de",
-          "URL": ["not available"]
+          'title': 'Yuranga',
+          'dates': '12/07 - 08/09',
+          'description': 'Launching the new CMS called TeamSite for BR.de',
+          'images': 'images/yuranga.jpg'
       },
       {
-          "title": "Sophora",
-          "dates": "12/09 - 02/11",
-          "description": "Launching the new CMS called Sophora for BR.de",
-          "URL": ["http://www.br.de"]
+          'title': 'Sophora',
+          'dates': '12/09 - 02/11',
+          'description': 'Launching the new CMS called Sophora for BR.de',
+          'images': 'images/sophora.jpg'
       },
       {
-          "title": "BR-Mediathek",
-          "dates": "10/11 - 04/13",
-          "description": "Hosting all the BR Video- and Audio-Content on its own plattform",
-          "URL": ["http://www.br.de/mediathek/video/index.html"]
+          'title': 'BR-Mediathek',
+          'dates': '10/11 - 04/13',
+          'description': 'Hosting all the BR Video- and Audio-Content on its own plattform',
+          'images': 'images/br-mediathek.jpg'
       },
       {
-          "title": "BR2GO",
-          "dates": "08/13 - 04/15",
-          "description": "Transforming the huge www.BR.de into a modern responsive Website",
-          "URL": ["http://www.br.de/"]
+          'title': 'BR2GO',
+          'dates': '08/13 - 04/15',
+          'description': 'Transforming the huge www.BR.de into a modern responsive Website',
+          'images': 'images/br2go.jpg'
       }
   ],
   'display': function() {
@@ -122,7 +122,8 @@ var projects = {
       var project = this.projects[i];
       $(id).append(dataChange(project.title, HTMLprojectTitle))
            .append(dataChange(project.dates, HTMLprojectDates))
-           .append(dataChange(project.description, HTMLprojectDescription));
+           .append(dataChange(project.description, HTMLprojectDescription))
+           .append(dataChange(project.images, HTMLprojectImage));
 
     }
   }
@@ -131,24 +132,23 @@ var projects = {
 var education = {
     'schools': [
         {
-            "name": "Staatl. Realschule",
-            "location": "Vaterstetten",
-            "degree": "Mittl. Reife",
-            "major": [
-                "Mathematik, Englisch"
-            ],
-            "date": "1986",
-            "URL": "https://www.realschulebayern.de/index.php?id=224&Schulnr=0765"
-        },
-        {
             "name": "CDI",
             "location": "Munich",
             "degree": "Informatikkaufmann",
-            "major": [
+            "majors": [
                 "Economic, IT"
             ],
-            "date": "2000",
+            "dates": 2000,
             "URL": "http://www.multimedia.de/ausbildung/institution/id/711/"
+        }
+    ],
+
+    'onlineCourses': [
+        {
+            'title': 'Responsive Web Development',
+            'school': 'Macromedia',
+            'date': 2012,
+            'url': 'http://www.macromedia.de'
         }
     ],
     'display': function() {
@@ -158,9 +158,19 @@ var education = {
             var school = this.schools[i];
             $(id).append((dataChange(school.name, HTMLschoolName) + dataChange(school.degree, HTMLschoolDegree))
                 .replace('#', school.url))
-                .append(dataChange(school.date, HTMLschoolDates))
+                .append(dataChange(school.dates, HTMLschoolDates))
                 .append(dataChange(school.location, HTMLschoolLocation))
                 .append(dataChange(school.majors, HTMLschoolMajor));
+
+        for (var j in this.onlineCourses) {
+            $('#education').append(HTMLonlineClasses);
+            $('#education').append(dataChange(j, HTMLonlineStart));
+            var id = '#online-entry-' + j;
+            var online = this.onlineCourses[j];
+            $(id).append(dataChange(online.title, HTMLonlineTitle) + dataChange(online.school, HTMLonlineSchool))
+                .append(dataChange(online.date, HTMLonlineDates))
+                .append(dataChange(online.url, HTMLonlineURL).replace('#', online.url));
+            }
         }
     }
 };
